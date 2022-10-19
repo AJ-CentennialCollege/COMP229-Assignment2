2da92a37e5b53f86bbedd814e3b18878e0ff32c3
AJ-CentennialCollege <abalbaca@my.centennialcollege.ca>
2022-10-19 14:21:49 -0400

    Authorization Completed

--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -6,14 +6,15 @@ import { DisplayContactList,
         ProcessContactEditPage,
         DisplayContactEditPage,
         ProcessContactDelete } from "../controllers/contacts.controller.server.js";
+import { AuthGuard } from "../utils/index.js";
 
 const router = Router();
 
 router.get('/contact-list', DisplayContactList);
-router.get('/contact-add', DisplayContactAddPage);
-router.post('/contact-add', ProcessContactAddPage);
-router.post('/contact-edit/:id', ProcessContactEditPage);
-router.get('/contact-edit/:id', DisplayContactEditPage);
-router.get('/contact-delete/:id', ProcessContactDelete);
+router.get('/contact-add', AuthGuard, DisplayContactAddPage);
+router.post('/contact-add', AuthGuard, ProcessContactAddPage);
+router.post('/contact-edit/:id', AuthGuard, ProcessContactEditPage);
+router.get('/contact-edit/:id', AuthGuard, DisplayContactEditPage);
+router.get('/contact-delete/:id', AuthGuard, ProcessContactDelete);
 
 export default router;

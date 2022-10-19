2517b009f6e5ef96f80717fcb1b101f7279080ae
AJ-CentennialCollege <abalbaca@my.centennialcollege.ca>
2022-10-19 13:37:23 -0400

    CRUD Complete

--- app/controllers/contacts.controller.server.js
+++ app/controllers/contacts.controller.server.js
@@ -7,9 +7,74 @@ export function DisplayContactList(req, res, next) {
             res.end(err);
         }
 
-        res.render('index', {title: 'Contact List', page: '/contacts/list', contacts: contactsCollection});
+        res.render('index', {title: 'Contact List', page: 'contacts/list', contacts: contactsCollection});
     })
 }
 export function DisplayContactAddPage(req, res, next) {
     res.render('index', {title: 'Add Contact', page: 'contacts/edit', contact: {}});
+}
+
+export function ProcessContactAddPage(req, res, next){
+
+    let newContact = contactModel({
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email
+    });
+
+    contactModel.create(newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+export function DisplayContactEditPage(req, res, next){
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Edit Contact', page: 'contacts/edit', contact: contact });
+    });    
+}
+
+export function ProcessContactEditPage(req, res, next){
+
+    let id = req.params.id;
+
+    let newContact = contactModel({
+        _id: req.body.id,
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email
+    });
+
+    contactModel.updateOne({_id: id }, newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+export function ProcessContactDelete(req, res, next){
+    let id = req.params.id; 
+
+    contactModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list');
+    });
 }
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 
 import { DisplayContactList,
-        DisplayContactAddPage } from "../controllers/contacts.controller.server.js";
+        DisplayContactAddPage, 
+        ProcessContactAddPage,
+        ProcessContactEditPage,
+        DisplayContactEditPage,
+        ProcessContactDelete } from "../controllers/contacts.controller.server.js";
 
 const router = Router();
 
 router.get('/contact-list', DisplayContactList);
 router.get('/contact-add', DisplayContactAddPage);
+router.post('/contact-add', ProcessContactAddPage);
+router.post('/contact-edit/:id', ProcessContactEditPage);
+router.get('/contact-edit/:id', DisplayContactEditPage);
+router.get('/contact-delete/:id', ProcessContactDelete);
 
 export default router;

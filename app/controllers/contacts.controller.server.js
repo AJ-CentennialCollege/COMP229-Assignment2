7517f4d944f401bbe0d1d11ed7ab8fe141b5ea61
AJ-CentennialCollege <abalbaca@my.centennialcollege.ca>
2022-10-19 03:00:23 -0400

    Contact Add Page Completed

--- app/controllers/contacts.controller.server.js
+++ app/controllers/contacts.controller.server.js
@@ -10,3 +10,6 @@ export function DisplayContactList(req, res, next) {
         res.render('index', {title: 'Contact List', page: '/contacts/list', contacts: contactsCollection});
     })
 }
+export function DisplayContactAddPage(req, res, next) {
+    res.render('index', {title: 'Add Contact', page: 'contacts/edit', contact: {}});
+}
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { DisplayContactList } from "../controllers/contacts.controller.server.js";
+import { DisplayContactList,
+        DisplayContactAddPage } from "../controllers/contacts.controller.server.js";
 
 const router = Router();
 
 router.get('/contact-list', DisplayContactList);
+router.get('/contact-add', DisplayContactAddPage);
 
 export default router;
